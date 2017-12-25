245c91ba43405b3c53e8f89093ede855fe20db6f
RodoggX <mikerodolph@yahoo.com>
2017-12-25 09:48:29 -0500

    Add min magnitude preference Add settings activity

--- app/src/main/java/com/example/rodoggx/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/rodoggx/quakereport/EarthquakeActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -106,5 +108,22 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     public void onLoaderReset(Loader<List<Earthquake>> loader) {
         //clear the adapter
         adapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/rodoggx/quakereport/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.rodoggx.quakereport;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class EarthquakeFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
+    }
+}
